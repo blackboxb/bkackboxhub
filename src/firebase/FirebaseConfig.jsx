ab3f53086668c46e7208be093417ca068394d9eb
blackboxb <blackboxrgukt@gmail.com>
2025-03-05 00:26:17 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,14 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQsJ5MS4-MacHnO5wxQX8V2tGe0y6eCXo",
-  authDomain: "myecommerce2-7b8fc.firebaseapp.com",
-  projectId: "myecommerce2-7b8fc",
-  storageBucket: "myecommerce2-7b8fc.appspot.com",
-  messagingSenderId: "715233167432",
-  appId: "1:715233167432:web:3a0acec83b123399d3307d"
+  apiKey: "AIzaSyCoZitdM4QooW5AdfChkPySQ2hRhyXF4ls",
+  authDomain: "blackboxbot-c73a0.firebaseapp.com",
+  databaseURL: "https://blackboxbot-c73a0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "blackboxbot-c73a0",
+  storageBucket: "blackboxbot-c73a0.firebasestorage.app",
+  messagingSenderId: "1009093960990",
+  appId: "1:1009093960990:web:e2c1cea8803cebd2ef150f",
+  measurementId: "G-18FLDV3SKL"
 };
 
 // Initialize Firebase
