92adfacf949306907bc1504fb4cee446c675e971
blackboxb <blackboxrgukt@gmail.com>
2025-03-05 00:18:43 +0530

    Delete src directory

--- src/components/heroSection/HeroSection.jsx
+++
@@ -1,9 +0,0 @@
-const HeroSection = () => {
-    return (
-        <div>
-           <img className=" h-44 lg:h-full" src="../img/hero1.png" alt="" />
-        </div>
-    );
-}
-
-export default HeroSection;
